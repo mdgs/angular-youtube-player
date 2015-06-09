06594b4227a6dad8291ad83999ff311be183412e
madeagus <madeagus@gmail.com>
2015-06-08 23:00:00 -0400

    attach delegateService adopted from ionic. Needed to allow controller get youtube player object

---
+++ app/scripts/services/delegateService.js
@@ -0,0 +1,10 @@
+/* 
+* @Author: madeagus
+* @Date:   2015-06-08 21:28:11
+* @Last Modified by:   madeagus
+* @Last Modified time: 2015-06-08 22:44:46
+*/
+
+youtubePlayerModule.service('$youtubePlayerDelegate', base.DelegateService([
+  'getPlayer'
+]));
